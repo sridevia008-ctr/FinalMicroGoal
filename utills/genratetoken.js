cc00d29474d42c2cb49866450de5250ac25745e1
sridevia008-ctr <sridevia008@gmail.com>
2026-03-06 16:46:28 +0530

    Add files via upload

---
+++ utills/genratetoken.js
@@ -0,0 +1,9 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const genrateToken = (userId) => {
+  const token = jwt.sign({ userId: userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+  return token;
+};
+module.exports = genrateToken;
